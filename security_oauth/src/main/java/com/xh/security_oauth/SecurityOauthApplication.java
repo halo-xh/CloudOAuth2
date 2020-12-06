af28ba4b45e0d67d2386cf7d55506a291dbd39a9
xiaoh1005 <czxqxxxh@126.com>
2020-12-06 20:03:08 +0800

    init

---
+++ security_oauth/src/main/java/com/xh/security_oauth/SecurityOauthApplication.java
@@ -0,0 +1,13 @@
+package com.xh.security_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityOauthApplication.class, args);
+    }
+
+}
---
+++ security_source/src/test/java/com/xh/security_source/SecuritySourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xh.security_source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
