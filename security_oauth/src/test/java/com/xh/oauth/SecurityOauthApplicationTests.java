f46e81c4b1010ba51d0e93a93648afb50c03e3ea
xiaohong <345125740@qq.com>
2021-07-16 18:16:21 +0800

    add test

--- security_oauth/src/test/java/com/xh/oauth/SecurityOauthApplicationTests.java
+++ security_oauth/src/test/java/com/xh/oauth/SecurityOauthApplicationTests.java
@@ -1,13 +1,39 @@
 package com.xh.oauth;
 
+import com.xh.oauth.clients.entity.MyClientDetails;
+import com.xh.oauth.clients.repo.MyClientDetailsRepository;
+import com.xh.oauth.utils.YesOrNoEnum;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class SecurityOauthApplicationTests {
 
+    @Autowired
+    private MyClientDetailsRepository clientDetailsRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Test
     void contextLoads() {
+        String admin = passwordEncoder.encode("admin");
+        MyClientDetails myClientDetails = new MyClientDetails();
+        myClientDetails.setClientSecret(admin);
+        myClientDetails.setAuthorities("13,123,123");
+        myClientDetails.setClientId("admin");
+        myClientDetails.setAutoApproveScopes(YesOrNoEnum.NO);
+        myClientDetails.setAccessTokenValiditySeconds(10000);
+        myClientDetails.setScope("test");
+        myClientDetails.setRefreshTokenValiditySeconds(1000);
+        myClientDetails.setRegisteredRedirectUris("www.baidu.com");
+        myClientDetails.setAuthorizedGrantTypes("authorization_code");
+        myClientDetails.setResourceIds("2131");
+        clientDetailsRepository.save(myClientDetails);
+
     }
 
 }
